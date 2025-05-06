aa1eb0c94dfbddd36487c45c1e283df439f09469
Sevda Abbasi <sevdaabbasi37@gmail.com>
2025-05-06 13:20:41 +0300

    Add FeedViewController

--- SocialMediaApp/FeedViewController.swift
+++ SocialMediaApp/FeedViewController.swift
@@ -6,86 +6,42 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseStorage
 
-class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    
+
     @IBOutlet weak var tableView: UITableView!
+    
+    var userEmailArray = [String]()
+    var commentArray = [String]()
+    var userImageArray = [String]()
+    var likeArray = [Int]()
+    var documentIdArray = [String]()
+    
+   
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return userEmailArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", )
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.commentLabel.text = commentArray[indexPath.row]
+        cell.documentIdLabel.text = documentIdArray[indexPath.row]
+        cell.userImageView.sd_setImage(with: URL(string: self.userImageArray[indexPath.row]))
     }
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
-
-
-
-/*
- import UIKit
- import Firebase
-
- class FeedCell: UITableViewCell {
-
-     @IBOutlet weak var userEmailLabel: UILabel!
-     
-     @IBOutlet weak var commentLabel: UILabel!
-     
-     @IBOutlet weak var userImageView: UIImageView!
-     
-     @IBOutlet weak var likeLabel: UILabel!
-     
-     @IBOutlet weak var documentIdLabel: UILabel!
-     
-     override func awakeFromNib() {
-         super.awakeFromNib()
-         // Initialization code
-     }
-
-     override func setSelected(_ selected: Bool, animated: Bool) {
-         super.setSelected(selected, animated: animated)
-
-         // Configure the view for the selected state
-     }
-     
-     @IBAction func likeButtonClicked(_ sender: Any) {
-         
-         let fireStoreDatabase = Firestore.firestore()
-         
-         if let likeCount = Int(likeLabel.text!) {
-             
-             let likeStore = ["likes" : likeCount + 1] as [String : Any]
-             
-             fireStoreDatabase.collection("Posts").document(documentIdLabel.text!).setData(likeStore, merge: true)
-
-         }
-         
-         
-     }
-     
- }
-
-
-*/
