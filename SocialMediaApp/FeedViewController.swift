edd7b0e0c254d3c68323c43b622a41f7e363eda6
Sevda Abbasi <sevdaabbasi37@gmail.com>
2025-05-06 12:33:24 +0300

    update

--- SocialMediaApp/FeedViewController.swift
+++ SocialMediaApp/FeedViewController.swift
@@ -7,7 +7,20 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", )
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,3 +40,52 @@ class FeedViewController: UIViewController {
     */
 
 }
+
+
+
+/*
+ import UIKit
+ import Firebase
+
+ class FeedCell: UITableViewCell {
+
+     @IBOutlet weak var userEmailLabel: UILabel!
+     
+     @IBOutlet weak var commentLabel: UILabel!
+     
+     @IBOutlet weak var userImageView: UIImageView!
+     
+     @IBOutlet weak var likeLabel: UILabel!
+     
+     @IBOutlet weak var documentIdLabel: UILabel!
+     
+     override func awakeFromNib() {
+         super.awakeFromNib()
+         // Initialization code
+     }
+
+     override func setSelected(_ selected: Bool, animated: Bool) {
+         super.setSelected(selected, animated: animated)
+
+         // Configure the view for the selected state
+     }
+     
+     @IBAction func likeButtonClicked(_ sender: Any) {
+         
+         let fireStoreDatabase = Firestore.firestore()
+         
+         if let likeCount = Int(likeLabel.text!) {
+             
+             let likeStore = ["likes" : likeCount + 1] as [String : Any]
+             
+             fireStoreDatabase.collection("Posts").document(documentIdLabel.text!).setData(likeStore, merge: true)
+
+         }
+         
+         
+     }
+     
+ }
+
+
+*/
